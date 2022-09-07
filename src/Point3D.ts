eedf24d3f8a7b8931776be6602db1e8140491dab
Kyle Villeneuve <kylevilleneuve@gmail.com>
2022-09-07 11:57:46 -0500

    Update Point3D.ts

--- src/Point3D.ts
+++ src/Point3D.ts
@@ -1,4 +1,4 @@
-import Point2D from "./Point2D";
+import Point2D from './Point2D';
 
 class Point3D extends Point2D {
   z: number;
@@ -6,6 +6,28 @@ class Point3D extends Point2D {
   constructor(x = 0, y = 0, z = 0) {
     super(x, y);
     this.z = z;
+  }
+
+  clone() {
+    return new Point3D(this.x, this.y, this.z);
+  }
+
+  add(x: number, y = x, z = x) {
+    this.x += x;
+    this.y += y;
+    this.z += z;
+    return this;
+  }
+
+  addPoint(p: Point3D) {
+    return this.add(p.x, p.y, p.z);
+  }
+
+  multiply(x: number, y = x, z = x) {
+    this.x *= x;
+    this.y *= y;
+    this.z *= z;
+    return this;
   }
 }
 
